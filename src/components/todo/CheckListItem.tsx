f24c086787746d04b36c195f72f726d1f79edf09
“codnjs3575” <codnjs3575@naver.com>
2025-07-12 16:08:04 +0900

    fix&docs: 공용 컴포넌트 수정 및 주석 추가

--- src/components/todo/CheckListItem.tsx
+++ src/components/todo/CheckListItem.tsx
@@ -1,3 +1,11 @@
+/**
+ * CheckListItem 컴포넌트
+ * - 'todo' 또는 'done' 타입에 따라 다른 스타일과 기능을 렌더링
+ 
+ * - layout이 'default'인 경우 상세 페이지 이동 가능한 메인 페이지
+ * - layout이 'detail'인 경우 사용자가 내용을 직접 수정할 수 있는 상세 페이지
+ */
+
 import { TodoData } from '@/types/todo'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
@@ -25,59 +33,62 @@ export default function CheckListItem({
   const router = useRouter()
 
   return (
+    // 반응형 css (분기: isTodo, isDefault)
     <div
       className={`
-        flex w-full h-[50px] pl-3 border-2 border-slate9 items-center
+        flex w-full h-[50px] pl-3 border-2 border-slate9 items-center justify-center gap-4
         ${isTodo ? 'bg-white' : isDefault ? 'bg-violet1' : 'bg-violet-200'}
         ${
           isDefault
             ? 'h-[50px] rounded-[27px] mt-4'
-            : 'sm:w-full h-16 rounded-[24px] justify-center mx-auto items-center'
+            : 'sm:w-full h-16 rounded-[24px] mx-auto'
         }
     `}
     >
-      <div className="flex items-center justify-center w-full gap-4">
-        <div className="w-8 h-8 shrink-0">
-          <button
-            className={`cursor-pointer w-full h-full flex items-center justify-center rounded-full`}
-            onClick={onClick ? () => onClick(todo) : undefined}
-          >
-            <Image
-              src={
-                isTodo
-                  ? '/images/button/CheckBtnTodo.svg'
-                  : '/images/button/CheckBtnDone.svg'
-              }
-              alt="체크 버튼"
-              width={32}
-              height={32}
-            />
-          </button>
-        </div>
-        {isDefault ? (
-          <p
-            className={`cursor-pointer break-words whitespace-normal overflow-x-hidden text-ellipsis line-clamp-1 pr-3 text-slate8 flex-1
+      {/* 할 일 완료 토글 버튼 영역 */}
+      <div className="w-8 h-8 shrink-0">
+        <button
+          className={`cursor-pointer w-full h-full flex items-center justify-center rounded-full`}
+          onClick={onClick ? () => onClick(todo) : undefined}
+        >
+          <Image
+            src={
+              isTodo
+                ? '/images/button/CheckBtnTodo.svg'
+                : '/images/button/CheckBtnDone.svg'
+            }
+            alt="체크 버튼"
+            width={32}
+            height={32}
+          />
+        </button>
+      </div>
+
+      {/* 할 일 텍스트 영역 */}
+      {/* isDefault(메인페이지)라면 p 태그, !isDefault(상세페이지)라면 input 태그 렌더링 */}
+      {isDefault ? (
+        <p
+          className={`cursor-pointer break-words whitespace-normal overflow-x-hidden text-ellipsis line-clamp-1 pr-3 text-slate8 flex-1
           ${isTodo ? '' : 'line-through'} 
           `}
-            onClick={() => {
-              if (isDefault) router.push(`/items/${todo.id}`)
-            }}
-          >
-            {todo.name}
-          </p>
-        ) : (
-          <input
-            type="text"
-            id="userTodo"
-            style={{ width: `${(userTodo || todo.name).length + 10}ch` }}
-            value={userTodo !== undefined ? userTodo : todo.name}
-            className=" pr-3 text-slate9 font-bold text-xl leading-[23px] bg-transparent outline-none underline w-auto min-w-0 max-w-none"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              if (setUserTodo) setUserTodo(e.target.value)
-            }}
-          />
-        )}
-      </div>
+          onClick={() => {
+            if (isDefault) router.push(`/items/${todo.id}`)
+          }}
+        >
+          {todo.name}
+        </p>
+      ) : (
+        <input
+          type="text"
+          id="userTodo"
+          style={{ width: `${(userTodo || todo.name).length + 2}ch` }}
+          value={userTodo !== undefined ? userTodo : todo.name}
+          className=" pr-3 text-slate9 font-bold text-xl leading-[23px]  outline-none underline w-auto min-w-0 max-w-none bg-transparent"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            if (setUserTodo) setUserTodo(e.target.value)
+          }}
+        />
+      )}
     </div>
   )
 }
